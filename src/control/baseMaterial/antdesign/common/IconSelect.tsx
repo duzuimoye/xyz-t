27f9ce73030252952a2cc9f9e14074949e54c021
uxo2 <1748416084@qq.com>
2021-08-17 07:17:41 +0800

    style update

--- src/control/baseMaterial/antdesign/common/IconSelect.tsx
+++ src/control/baseMaterial/antdesign/common/IconSelect.tsx
@@ -21,6 +21,16 @@ export default ({
   console.log(customIcon, selectIcon)
 
   const DrawerIconSelect = () => {
+    const drawerHeaderStyle = {
+      padding: '10px',
+      height: '40px',
+      backgroundColor: '#3c3c3c',
+      borderBottom: '1px solid 333f57',
+      borderRadius: 0
+    }
+    const drawerBodyStyle = {
+      padding: '0 5px 5px'
+    }
     return (
       <Drawer
         placement="right"
@@ -28,16 +38,8 @@ export default ({
         visible={visible}
         closable={false}
         title={<div style={{ color: '#fff' }}>图标选择</div>}
-        headerStyle={{
-          padding: '10px',
-          height: '40px',
-          backgroundColor: '#3c3c3c',
-          borderBottom: '1px solid 333f57',
-          borderRadius: 0
-        }}
-        bodyStyle={{
-          padding: '0 5px 5px'
-        }}
+        headerStyle={drawerHeaderStyle}
+        bodyStyle={drawerBodyStyle}
         onClose={() => {
           setVisible(false)
           setCustomIcon(icon)
@@ -51,7 +53,7 @@ export default ({
           paddingBottom: '10px',
           marginBottom: '10px'
         }}>
-          <LineBlockLabel>图标</LineBlockLabel>
+          <LineBlockLabel>图标:</LineBlockLabel>
           <Radio.Group
             size="small"
             value={iconType}
